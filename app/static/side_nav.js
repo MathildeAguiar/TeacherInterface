fef27db308dca3c9c7360b1f50e2a0681a60a393
Mathilde Aguiar <mathilde.aguiar@etu.univ-grenoble-alpes.fr>
2021-06-01 18:05:45 +0200

    side nav is fixed git add --all!

---
+++ app/static/side_nav.js
@@ -0,0 +1,19 @@
+/* global bootstrap: false */
+(function () {
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  })()
+
+  
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
