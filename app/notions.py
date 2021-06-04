06a1521ac2a03e809ad99f564a4f8f93a137692a
Mathilde Aguiar <mathilde.aguiar@etu.univ-grenoble-alpes.fr>
2021-06-04 09:46:05 +0200

    model added

---
+++ app/notions.py
@@ -0,0 +1,8 @@
+class Notion(object):
+    def __init__(self) :
+        self.id = 1
+        self.text = "text"
+        self.author = "auth"
+        self.category = "cat"
+        self.draft = "dra"
+        self.create_time = "time"
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for, redirect
 from .forms import ResearchForm
 from .creation_exo import CreaExo
 from .validation import TxtBrowser
+from .notions import Notion
 #from .models import select_exo, select_student, create_connection, select_notion
 
 
@@ -41,16 +42,30 @@ def table():
 def creation_exo():
     form = CreaExo()
     if form.validate_on_submit():
-        return redirect(url_for('table')) #change
+        return redirect(url_for('list_exo')) #change
     return render_template(
         'creation_exo.html',
         form = form
     )
 
+@app.route('/list_exo/', methods=["GET", "POST"])
+def list_exo():
+   
+    return render_template(
+        'list_exo.html'
+    )
 
 @app.route('/validation/', methods=["GET", "POST"])
 def validation():
     form = TxtBrowser()
+    notions = Notion()
+    titles = [('id', '#'), ('text', 'Message'), ('author', 'Author'), ('category', 'Category'), ('draft', 'Draft'), ('create_time', 'Create Time')]
+    
+    #pagination (need SQLAlchemy)
+    #page = request.args.get('page', 1, type=int)
+    #pagination = Message.query.paginate(page, per_page=30)
+    #notions = pagination.items
+
     #if form.validate_on_submit():
         #return redirect(url_for('va')) #change
         #if we validate this we stay on the same page and we have new things that appear 
@@ -58,8 +73,21 @@ def validation():
 
     return render_template(
         'validation.html',
-        form = form
+        form = form,
+        #for the table 
+        notions = notions, 
+        titles = titles
+        #pagination = pagination
     )
+
+@app.route('/connexion/', methods=["GET", "POST"])
+def connexion():
+   
+    return render_template(
+        'connexion.html'
+    )
+
+
 
 if __name__ == "__main__":
     app.run()
